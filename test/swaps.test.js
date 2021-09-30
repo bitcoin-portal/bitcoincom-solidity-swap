826903788ea601f8d47f29e554970bc1cb1694e0
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2021-09-30 10:29:38 +0700

    🧪 pairCodeHash test added

--- test/swaps.test.js
+++ test/swaps.test.js
@@ -1,7 +1,7 @@
-const Factory = artifacts.require("SwapsFactory");
-const Router = artifacts.require("SwapsRouter");
 const Token = artifacts.require("Token");
 const Weth = artifacts.require("WrappedEther");
+const Router = artifacts.require("SwapsRouter");
+const Factory = artifacts.require("SwapsFactory");
 
 const catchRevert = require("./exceptionsHelpers.js").catchRevert;
 
@@ -9,11 +9,11 @@ require("./utils");
 
 const BN = web3.utils.BN;
 
-// TESTING PARAMETERS
-const SECONDS_IN_DAY = 30;
+const FOUR_ETH = web3.utils.toWei("4");
 const FIVE_ETH = web3.utils.toWei("5");
+const NINE_ETH = web3.utils.toWei("9");
+
 const STATIC_SUPPLY = web3.utils.toWei("5000000");
-const WETH_ADDRESS = "0x...."
 
 const getLastEvent = async (eventName, instance) => {
     const events = await instance.getPastEvents(eventName, {
@@ -25,6 +25,7 @@ const getLastEvent = async (eventName, instance) => {
 
 contract("Swaps", ([owner, user1, user2, random]) => {
 
+    let weth;
     let factory;
     let router;
     let launchTime;
@@ -45,6 +46,15 @@ contract("Swaps", ([owner, user1, user2, random]) => {
             assert.equal(
                 feeToSetterAddress,
                 owner
+            );
+        });
+
+        it("should have correct pairCodeHash value", async () => {
+            const pairCodeHash = await factory.pairCodeHash();
+            const expectedValue = '0xe24fce0a3cc0ec9e09f37d8b9aa380dbd0ee348a6b1f2ae2378ac1414c8e8390';
+            assert.equal(
+                pairCodeHash,
+                expectedValue
             );
         });
     });
