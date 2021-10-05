f6c132cdbc1846a5709428072754f71755fccfc9
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2021-10-05 11:06:27 +0700

    🧪 latest tests after demo-session

--- test/token.test.js
+++ test/token.test.js
@@ -1,5 +1,4 @@
 const Token = artifacts.require("Token");
-
 const catchRevert = require("./exceptionsHelpers.js").catchRevert;
 
 require("./utils");
@@ -7,7 +6,7 @@ require("./utils");
 const BN = web3.utils.BN;
 
 // TESTING PARAMETERS
-const SECONDS_IN_DAY = 30;
+const ONE_TOKEN = web3.utils.toWei("1");
 const FIVE_ETH = web3.utils.toWei("5");
 const STATIC_SUPPLY = web3.utils.toWei("5000000");
 
@@ -19,12 +18,12 @@ const getLastEvent = async (eventName, instance) => {
     return events.pop().returnValues;
 };
 
-contract("Token", ([owner, user1, user2, random]) => {
+contract("Token", ([owner, alice, bob, random]) => {
 
     let token;
     let launchTime;
 
-    before(async () => {
+    beforeEach(async () => {
         token = await Token.new();
     });
 
@@ -51,6 +50,100 @@ contract("Token", ([owner, user1, user2, random]) => {
             assert.equal(
                 decimals,
                 18
+            );
+        });
+    });
+
+    describe.only("Token Transfer Functionality", () => {
+
+        it("should transfer correct amount from walletA to walletB", async () => {
+
+            const transferValue = ONE_TOKEN;
+            const balanceBefore = await token.balanceOf(bob);
+
+            await token.transfer(
+                bob,
+                transferValue,
+                {
+                    from: owner
+                }
+            );
+
+            const balanceAfter = await token.balanceOf(bob);
+
+            assert.equal(
+                parseInt(balanceAfter),
+                parseInt(balanceBefore) + parseInt(transferValue)
+            );
+        });
+
+        it("should revert if not enough balance in the wallet", async () => {
+
+            const balanceBefore = await token.balanceOf(alice);
+
+            await catchRevert(
+                token.transfer(
+                    bob,
+                    parseInt(balanceBefore) + 1,
+                    {
+                        from: alice
+                    }
+                )
+            );
+        });
+
+        it("should reduce wallets balance after transfer", async () => {
+
+            const transferValue = ONE_TOKEN;
+            const balanceBefore = await token.balanceOf(owner);
+
+            await token.transfer(
+                bob,
+                transferValue,
+                {
+                    from: owner
+                }
+            );
+
+            const balanceAfter = await token.balanceOf(owner);
+
+            assert.equal(
+                parseInt(balanceAfter),
+                parseInt(balanceBefore) - parseInt(transferValue)
+            );
+        });
+
+        it("should emit correct Transfer event", async () => {
+
+            const transferValue = ONE_TOKEN;
+            const expectedRecepient = bob;
+
+            await token.transfer(
+                expectedRecepient,
+                transferValue,
+                {
+                    from: owner
+                }
+            );
+
+            const { from, to, value } = await getLastEvent(
+                "Transfer",
+                token
+            );
+
+            assert.equal(
+                from,
+                owner
+            );
+
+            assert.equal(
+                to,
+                expectedRecepient
+            );
+
+            assert.equal(
+                value,
+                transferValue
             );
         });
     });
