5f9407f7565fbca122b391f4480459555efa6782
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-05-06 22:12:51 +0700

    add interface for ISwapsRouter

---
+++ contracts/ISwapsRouter.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BCOM
+
+pragma solidity ^0.8.19;
+
+interface ISwapsRouter {
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint[] memory amounts
+        );
+}
