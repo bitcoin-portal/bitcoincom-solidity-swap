aa938cb2e34fb495d9f4c4ab525a27a78b96ccb9
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-05-17 02:58:50 +0700

    create fork-live tests looping cycles

--- test/maker.test.js
+++ test/maker.test.js
@@ -1,15 +1,26 @@
-const Maker = artifacts.require("LiquidityMaker");
+const Token = artifacts.require("Token");
 const Weth = artifacts.require("WrappedEther");
+const Maker = artifacts.require("LiquidityMaker");
+const Factory = artifacts.require("ISwapsFactory");
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
 // const catchRevert = require("./exceptionsHelpers.js").catchRevert;
 const { expectRevert } = require('@openzeppelin/test-helpers');
 require("./utils");
 
 // const BN = web3.utils.BN;
 
-const ONE_ETH = web3.utils.toWei("1");
-const FOUR_ETH = web3.utils.toWei("4");
-const FIVE_ETH = web3.utils.toWei("5");
-const NINE_ETH = web3.utils.toWei("9");
+const tokens = (value) => {
+    return web3.utils.toWei(value);
+}
+
+const ONE_ETH = tokens("1");
+const FOUR_ETH = tokens("4");
+const FIVE_ETH = tokens("5");
+const NINE_ETH = tokens("9");
+
+const HALF_ETH = tokens("0.5");
+
+const testBlock = 17274000;
 
 const getLastEvent = async (eventName, instance) => {
     const events = await instance.getPastEvents(eventName, {
@@ -19,15 +30,43 @@ const getLastEvent = async (eventName, instance) => {
     return events.pop().returnValues;
 };
 
-contract("Maker", ([owner, alice, bob, random]) => {
 
-    const MAINNET_ROUTER = "0xB4B0ea46Fe0E9e8EAB4aFb765b527739F2718671";
-    const MAINNET_FACTORY = "0xee3E9E46E34a27dC755a63e2849C9913Ee1A06E2";
-    const MAINNET_WRAPPED_ETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-    const MAINNET_VERSE_TOKEN = "0x249cA82617eC3DfB2589c4c17ab7EC9765350a18";
+const MAINNET_ROUTER = "0xB4B0ea46Fe0E9e8EAB4aFb765b527739F2718671";
+const MAINNET_FACTORY = "0xee3E9E46E34a27dC755a63e2849C9913Ee1A06E2";
+const MAINNET_WRAPPED_ETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const MAINNET_VERSE_TOKEN = "0x249cA82617eC3DfB2589c4c17ab7EC9765350a18";
+const MAINNET_DAI_TOKEN = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const MAINNET_DEV_ADDRESS = "0x641AD78BAca220C5BD28b51Ce8e0F495e85Fe689";
 
-    let maker;
+const getSomeDai = async (
+    beneficiary,
+    amount
+) => {
+    await helpers.impersonateAccount(
+        MAINNET_DEV_ADDRESS
+    );
+
+    const impersonatedSigner = await ethers.getSigner(
+        MAINNET_DEV_ADDRESS
+    );
+
+    let daiEthers = await ethers.getContractAt(
+        "Token",
+        MAINNET_DAI_TOKEN
+    );
+
+    await daiEthers.connect(impersonatedSigner).transfer(
+        beneficiary,
+        amount
+    );
+}
+
+contract("LiquidityMaker", ([owner, alice, bob, random]) => {
+
+    let dai;
     let weth;
+    let maker;
+    let verse;
 
     beforeEach(async () => {
         maker = await Maker.new(
@@ -37,6 +76,14 @@ contract("Maker", ([owner, alice, bob, random]) => {
 
         weth = await Weth.at(
             MAINNET_WRAPPED_ETH
+        );
+
+        dai = await Token.at(
+            MAINNET_DAI_TOKEN
+        );
+
+        verse = await Token.at(
+            MAINNET_VERSE_TOKEN
         );
     });
 
@@ -61,17 +108,18 @@ contract("Maker", ([owner, alice, bob, random]) => {
 
     describe("Initial liquidity functionality", () => {
 
-        it("should be able to provide liquidity", async () => {
+        it("should be able to provide liquidity (WETH/DAI) using WEHT", async () => {
 
             const depositor = alice;
-            const amount = FOUR_ETH;
+            const depositAmountEth = ONE_ETH;
+            const minDaiExpected = tokens("900");
 
             const balanceBefore = await weth.balanceOf(
                 depositor
             );
 
             await weth.send(
-                amount,
+                depositAmountEth,
                 {
                     from: depositor
                 }
@@ -87,22 +135,68 @@ contract("Maker", ([owner, alice, bob, random]) => {
 
             assert.equal(
                 balanceChange.toString(),
-                amount.toString()
+                depositAmountEth.toString()
             );
 
             await weth.approve(
                 maker.address,
-                ONE_ETH,
+                depositAmountEth,
                 {
                     from: depositor
                 }
             );
 
-            await maker.makeLiquidity(
+            await maker.makeLiquidityDual(
                 MAINNET_WRAPPED_ETH,
-                MAINNET_VERSE_TOKEN,
-                ONE_ETH,
-                1,
+                MAINNET_DAI_TOKEN,
+                depositAmountEth,
+                minDaiExpected,
+                0,
+                0,
+                {
+                    from: depositor
+                }
+            );
+
+            const swapResultData = await getLastEvent(
+                "SwapResults",
+                maker
+            );
+
+            console.log(swapResultData, 'swapResultData');
+
+            const liquidityData = await getLastEvent(
+                "LiquidityAdded",
+                maker
+            );
+
+            console.log(liquidityData, 'liquidityData');
+        });
+
+        it("should be able to provide liquidity (WETH/DAI) using DAI", async () => {
+
+            const depositor = alice;
+            const depositAmountDai = tokens("1820");
+            const minEthExpected = HALF_ETH;
+
+            await getSomeDai(
+                depositor,
+                depositAmountDai
+            );
+
+            await dai.approve(
+                maker.address,
+                depositAmountDai,
+                {
+                    from: depositor
+                }
+            );
+
+            await maker.makeLiquidityDual(
+                MAINNET_DAI_TOKEN,
+                MAINNET_WRAPPED_ETH,
+                depositAmountDai,
+                minEthExpected,
                 0,
                 0,
                 {
