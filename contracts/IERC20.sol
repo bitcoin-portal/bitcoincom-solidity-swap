71205f35b2371032f63a4d8620a6e2d4213e65fa
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2024-03-17 00:53:11 +0700

    interface update

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -11,9 +11,23 @@ interface IERC20 {
         view
         returns (uint256);
 
+    function transfer(
+        address _to,
+        uint256 _value
+    )
+        external
+        returns (bool);
+
     function transferFrom(
         address _from,
         address _to,
+        uint256 _value
+    )
+        external
+        returns (bool);
+
+    function approve(
+        address _spender,
         uint256 _value
     )
         external
--- contracts/ISwapsRouter.sol
+++ contracts/ISwapsRouter.sol
@@ -36,4 +36,55 @@ interface ISwapsRouter {
         returns (
             uint[] memory amounts
         );
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB
+        );
+
+    function getReserves(
+        address factory,
+        address tokenA,
+        address tokenB
+    )
+        external
+        view
+        returns (
+            uint112 reserveA,
+            uint112 reserveB,
+            uint32 blockTimestampLast
+        );
+
+    function FACTORY()
+        external
+        view
+        returns (address);
+
+    function quote(
+        uint256 amountA,
+        uint256 reserveA,
+        uint256 reserveB
+    )
+        external
+        pure
+        returns (uint256 amountB);
+
+    function pairFor(
+        address factory,
+        address tokenA,
+        address tokenB
+    )
+        external
+        pure
+        returns (address);
 }
